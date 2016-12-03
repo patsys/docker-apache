28318e23d9e5eca72608d94b522302682cb48967
th <th@centos.patsys.local>
2016-12-03 06:30:35 +0100

    add test

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
-FROM alpine:latest
+FROM patsys/alpine-mybase
 MAINTAINER Patrick Weber <pat.weber91@gmail.com>
-COPY update.sh /etc/
-COPY start /etc/
+COPY index.php /tmp/
 COPY apache_setup.sh /etc/my_init.d/
+RUN chmod u+x /etc/my_init.d/apache_setup.sh
 RUN chmod u+x /etc/update.sh
-RUN chmod ugo+x /etc/start
-RUN mkdir -p /etc/my_init.d
 RUN mkdir /run/apache2/
 RUN mkdir /var/web
 RUN apk update && apk upgrade && apk add apache2 php5-apache2 && rm -rf /var/cache/apk/*
--- apache_setup.sh
+++ apache_setup.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 if [ ! -e /var/web/config/httpd.conf ]; then
-  sed -i "s#/etc/apache2/#/var/web/config/#" /etc/apache2/httpd.conf
-  sed -i "s#/var/www#/var/web/www#" /etc/apache2/httpd.conf
-  sed -i "s# modules/# ../www/modules/#" /etc/apache2/httpd.conf
+  mkdir -p /var/web/config
+  cp /etc/apache2/httpd.conf /var/web/config/  && cp /etc/apache2/mime.types /var/web/config/
+fi
+if [ ! -e /var/web/config/conf.d ]; then
   mkdir -p /var/web/config/conf.d
-  cp /etc/apache2/httpd.conf /var/web/config/ && cp -R /etc/apache2/conf.d/* /var/web/config/conf.d/ && cp /etc/apache2/mime.types /var/web/config/
+  cp -R /etc/apache2/conf.d/* /var/web/config/conf.d/
 fi
-if [ ! -e /var/web/www/modules ]; then
-  mkdir -p /var/web/www/modules
-  cp -R /var/www/modules/* /var/web/www/modules/
+if [ ! -e /var/web/www ]; then
+   mkdir -p /var/web/www/localhost/htdocs
+   cp /tmp/index.php /var/web/www/localhost/htdocs/
 fi
+sed -i "s#/etc/apache2/#/var/web/config/#" /var/web/config/httpd.conf
+sed -i "s#/var/www#/var/web/www#" /var/web/config/httpd.conf
+sed -i "s# modules/# ../../www/modules/#" /var/web/config/httpd.conf
+find /var/web/config/conf.d/ -type f -name "*.conf" -print0 | xargs -0 sed -i  "s# modules/# ../../www/modules/#"
 mkdir -p /var/web/www/logs/
 exec /usr/sbin/httpd -D FOREGROUND -f /var/web/config/httpd.conf
